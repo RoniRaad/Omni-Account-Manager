14b55f1989607ad6a7b530a36c785c8820cd8e82
Roni <rraad004@fiu.edu>
2022-02-19 19:56:20 -0500

    Add project files.

---
+++ AccountManager/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AccountManager;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
