f5c09ced3db47a6cff2ed2ff4c3b7b068aa4781a
Roni <rraad004@fiu.edu>
2022-02-20 21:01:43 -0500

    Added some league login functionality.

---
+++ AccountManager.Core/Interfaces/ILoginService.cs
@@ -0,0 +1,9 @@
+﻿using AccountManager.Core.Models;
+
+namespace AccountManager.Core.Interfaces
+{
+    public interface ILoginService
+    {
+        Task Login(Account account);
+    }
+}
---
+++ AccountManager.Core/Models/LeagueSignInRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace AccountManager.Core.Models
+{
+    public class LeagueSignInRequest
+    {
+        [JsonPropertyName("username")]
+        public string Username { get; set; }
+        [JsonPropertyName("password")]
+        public string Password { get; set; }
+        [JsonPropertyName("platformId")]
+        public string PlatformId { get; set; }
+    }
+}
