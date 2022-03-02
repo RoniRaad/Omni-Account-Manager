b63fabe0dcbc91ce75977e80b1884a0c45420cd4
Roni Raad <RoniRaad@users.noreply.github.com>
2022-03-01 19:19:02 -0500

    Added Riot Authentication and Valorant rank information.

--- AccountManager.Core/ViewModels/AccountListItemViewModel.cs
+++ AccountManager.Core/ViewModels/AccountListItemViewModel.cs
@@ -1,12 +1,7 @@
 ﻿using AccountManager.Core.Enums;
 using AccountManager.Core.Interfaces;
 using AccountManager.Core.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
 namespace AccountManager.Core.ViewModels
 {
