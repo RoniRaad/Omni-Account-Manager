ce93b21483f1d9ff747b24a11df343f9e5a1734e
Roni Raad <RoniRaad@users.noreply.github.com>
2022-03-08 17:48:42 -0500

    Some more cleanup.

--- AccountManager.Infrastructure/Clients/RiotClient.cs
+++ AccountManager.Infrastructure/Clients/RiotClient.cs
@@ -13,20 +13,22 @@ namespace AccountManager.Infrastructure.Clients
     {
         private string bearerToken = "";
         private string entitlementToken = "";
-        private HttpClient _httpClient;
-        private HttpClient GenerateClient()
+        private readonly HttpClient _httpClient;
+
+        public RiotClient(IHttpClientFactory httpClientFactory)
         {
-            var handler = new ClearanceHandler
-            {
-                MaxRetries = 2
-            };
+            _httpClient = httpClientFactory.CreateClient("CloudflareBypass");
 
-            var client = new HttpClient(handler);
-            client.DefaultRequestHeaders.TryAddWithoutValidation("X-Riot-ClientPlatform", "ew0KCSJwbGF0Zm9ybVR5cGUiOiAiUEMiLA0KCSJwbGF0Zm9ybU9TIjogIldpbmRvd3MiLA0KCSJwbGF0Zm9ybU9TVmVyc2lvbiI6ICIxMC4wLjE5MDQyLjEuMjU2LjY0Yml0IiwNCgkicGxhdGZvcm1DaGlwc2V0IjogIlVua25vd24iDQp9");
-            client.DefaultRequestHeaders.TryAddWithoutValidation("X-Riot-ClientVersion", "release-04.03-shipping-6-671292");
-
-            return client;
         }
+
+        private async Task AddHeadersToClient()
+        {
+            if (_httpClient.DefaultRequestHeaders.Contains("X-Riot-ClientVersion"))
+                return;
+            _httpClient.DefaultRequestHeaders.TryAddWithoutValidation("X-Riot-ClientPlatform", "ew0KCSJwbGF0Zm9ybVR5cGUiOiAiUEMiLA0KCSJwbGF0Zm9ybU9TIjogIldpbmRvd3MiLA0KCSJwbGF0Zm9ybU9TVmVyc2lvbiI6ICIxMC4wLjE5MDQyLjEuMjU2LjY0Yml0IiwNCgkicGxhdGZvcm1DaGlwc2V0IjogIlVua25vd24iDQp9");
+            _httpClient.DefaultRequestHeaders.TryAddWithoutValidation("X-Riot-ClientVersion", await GetExpectedClientVersion());
+        }
+
         public async Task<string> GetExpectedClientVersion()
         {
             var response = await _httpClient.GetFromJsonAsync<ExpectedClientVersionResponse>("https://valorant-api.com/v1/version");
@@ -34,16 +36,16 @@ namespace AccountManager.Infrastructure.Clients
         }
         public async Task<string> GetToken(string username, string pass)
         {
-            var client = GenerateClient();
+            await AddHeadersToClient();
 
-            await client.PostAsJsonAsync("https://auth.riotgames.com/api/v1/authorization", new AuthRequestPostResponse {
+            await _httpClient.PostAsJsonAsync("https://auth.riotgames.com/api/v1/authorization", new AuthRequestPostResponse {
                 Id = "play-valorant-web-prod",
                 Nonce = "1",
                 RedirectUri = "https://playvalorant.com/opt_in",
                 ResponseType = "token id_token"
             });
 
-            var authResponse = await client.PutAsJsonAsync("https://auth.riotgames.com/api/v1/authorization", new AuthRequest
+            var authResponse = await _httpClient.PutAsJsonAsync("https://auth.riotgames.com/api/v1/authorization", new AuthRequest
             {
                 Type = "auth",
                 Username = username,
@@ -58,10 +60,10 @@ namespace AccountManager.Infrastructure.Clients
         }
         public async Task<string> GetEntitlementToken(string token)
         {
-            var client = GenerateClient();
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            await AddHeadersToClient();
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
 
-            var entitlementResponse = await client.PostAsJsonAsync("https://entitlements.auth.riotgames.com/api/token/v1", new { });
+            var entitlementResponse = await _httpClient.PostAsJsonAsync("https://entitlements.auth.riotgames.com/api/token/v1", new { });
             var entitlementResponseDeserialized = await entitlementResponse.Content.ReadFromJsonAsync<EntitlementTokenResponse>();
 
             return entitlementResponseDeserialized.EntitlementToken;
@@ -74,22 +76,20 @@ namespace AccountManager.Infrastructure.Clients
         public async Task<string> GetPuuId(string username, string password)
         {
             await GetAuth(username, password);
-            var client = GenerateClient();
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearerToken);
-            client.DefaultRequestHeaders.TryAddWithoutValidation("X-Riot-Entitlements-JWT", entitlementToken);
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearerToken);
+            _httpClient.DefaultRequestHeaders.TryAddWithoutValidation("X-Riot-Entitlements-JWT", entitlementToken);
 
-            var response = await client.GetFromJsonAsync<UserInfoResponse>("https://auth.riotgames.com/userinfo");
+            var response = await _httpClient.GetFromJsonAsync<UserInfoResponse>("https://auth.riotgames.com/userinfo");
             return response.PuuId;
         }
         public async Task<Rank> GetValorantRank(Account account)
         {
             int rankNumber;
             await GetAuth(account.Username, account.Password);
-            var client = GenerateClient();
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearerToken);
-            client.DefaultRequestHeaders.TryAddWithoutValidation("X-Riot-Entitlements-JWT", entitlementToken);
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearerToken);
+            _httpClient.DefaultRequestHeaders.TryAddWithoutValidation("X-Riot-Entitlements-JWT", entitlementToken);
 
-            var response = await client.GetFromJsonAsync<ValorantRankedResponse>($"https://pd.na.a.pvp.net/mmr/v1/players/{account.Id}");
+            var response = await _httpClient.GetFromJsonAsync<ValorantRankedResponse>($"https://pd.na.a.pvp.net/mmr/v1/players/{account.Id}");
 
             if (response?.QueueSkills?.Competitive?.TotalGamesNeededForRating > 0)
                 return new Rank() {
