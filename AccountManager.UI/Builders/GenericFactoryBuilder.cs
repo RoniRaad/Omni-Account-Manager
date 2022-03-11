b98a01e400a930b2b2880b7532f15e21d8f34eb6
Roni Raad <RoniRaad@users.noreply.github.com>
2022-03-11 00:04:31 -0500

    Moved to a wpf blazor solution due to issues with MAUI verison.

---
+++ AccountManager.UI/Builders/GenericFactoryBuilder.cs
@@ -0,0 +1,35 @@
+﻿using AccountManager.Core.Factories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountManager.UI.Builders
+{
+    public class GenericFactoryBuilder<TKey, TInterface> : IGenericFactoryBuilder<TKey, TInterface> where TKey : notnull, new()
+    {
+        private Dictionary<TKey, Type> _implementations { get; set; }
+        private ServiceCollection _services;
+        public GenericFactoryBuilder(ServiceCollection services)
+        {
+            _implementations = new Dictionary<TKey, Type>();
+            _services = services;
+        }
+        public IGenericFactoryBuilder<TKey, TInterface> AddImplementation<TImplementation>(TKey key) where TImplementation : class, TInterface
+        {
+            _services.AddSingleton<TImplementation>();
+            _implementations.Add(key, typeof(TImplementation));
+            return this;
+        }
+
+        public void Build()
+        {
+            _services.AddSingleton(services =>
+            {
+                return new GenericFactory<TKey, TInterface>(_implementations, services);
+            });
+        }
+    }
+}
---
+++ AccountManager.UI/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using AccountManager.UI.Builders;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountManager.UI.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IGenericFactoryBuilder<TKey, TInterface> AddFactory<TKey, TInterface>(this ServiceCollection services) where TKey : notnull, new()
+        {
+            return new GenericFactoryBuilder<TKey, TInterface>(services);
+        }
+    }
+}
