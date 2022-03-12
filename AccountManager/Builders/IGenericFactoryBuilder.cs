b5db66f93cb9a68d9b340b82f47afb7083b981d4
Roni <rraad004@fiu.edu>
2022-03-12 17:16:20 -0500

    Removed unused files.

--- AccountManager/Builders/IGenericFactoryBuilder.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AccountManager.UI.Builders
-{
-    public interface IGenericFactoryBuilder<TKey, TInterface> where TKey : notnull, new()
-    {
-        IGenericFactoryBuilder<TKey, TInterface> AddImplementation<TImplementation>(TKey key) where TImplementation : class, TInterface;
-        void Build();
-    }
-}
