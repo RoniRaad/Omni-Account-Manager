1a616a2f7db60731de67a812b112f9ab057f5c66
Roni Raad <RoniRaad@users.noreply.github.com>
2022-05-31 00:10:33 -0400

    Fixed issues with graph updating strangely, and graph data issues.

---
+++ AccountManager.Core/Static/ObservableCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+
+namespace AccountManager.Core.Static
+{
+    public static class ObservableCollectionExtensions
+    {
+        public static void RemoveAll<T>(this ObservableCollection<T> collection,
+                                                            Func<T, bool> condition)
+        {
+            for (int i = collection.Count - 1; i >= 0; i--)
+            {
+                if (condition(collection[i]))
+                {
+                    collection.RemoveAt(i);
+                }
+            }
+        }
+        public static void AddRange<T>(this ObservableCollection<T> collection,
+                                                    IEnumerable<T> values)
+        {
+            var enumerator = values.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                collection.Add(enumerator.Current);
+            }
+        }
+    }
+}
