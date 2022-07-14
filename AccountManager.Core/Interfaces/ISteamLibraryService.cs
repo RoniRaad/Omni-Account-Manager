a243c3398ef584e4d63d8f78a3ed552a23deee6d
Roni Raad <RoniRaad@users.noreply.github.com>
2022-07-13 20:15:43 -0400

    Added finishing touches. Published.

--- AccountManager.Core/Interfaces/ISteamLibraryService.cs
+++ AccountManager.Core/Interfaces/ISteamLibraryService.cs
@@ -5,8 +5,8 @@ namespace AccountManager.Core.Interfaces
 {
     public interface ISteamLibraryService
     {
-        List<SteamGameManifest> GetGameManifests();
-        LibraryFoldersWrapper GetLibraryFolders();
+        bool TryGetGameManifests(out List<SteamGameManifest> gameManifests);
+        bool TryGetLibraryFolders(out LibraryFoldersWrapper? libraryFolders);
         bool TryGetSteamDirectory(out string steamDirectory);
         bool TryGetUserId(Account account, out string userId);
     }
