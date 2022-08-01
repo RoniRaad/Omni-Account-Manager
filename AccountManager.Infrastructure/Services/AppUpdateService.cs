1bf3868262b1b3ea631b84acf689fbf10cdedc11
RoniRaad <46434047+RoniRaad@users.noreply.github.com>
2022-07-31 21:58:38 -0400

    Added update popup when an update is available.

---
+++ AccountManager.Infrastructure/Services/AppUpdateService.cs
@@ -0,0 +1,68 @@
+﻿using AccountManager.Core.Models.AppSettings;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Squirrel;
+
+namespace AccountManager.Infrastructure.Services
+{
+    public class SquirrelAppUpdateService : IAppUpdateService
+    {
+        private readonly AboutEndpoints _endpoints;
+        private readonly ILogger<SquirrelAppUpdateService> _logger;
+        public SquirrelAppUpdateService(IOptions<AboutEndpoints> endpoints, ILogger<SquirrelAppUpdateService> logger)
+        {
+            _endpoints = endpoints.Value;
+            _logger = logger;
+        }
+
+        public async Task<bool> CheckForUpdate()
+        {
+            try
+            {
+                using (var manager = await UpdateManager.GitHubUpdateManager(_endpoints.Github))
+                {
+                    var updateInfo = await manager.CheckForUpdate();
+                    if (updateInfo.ReleasesToApply.Count > 0)
+                    {
+                        return true;
+                    }
+
+                    return false;
+                }
+            }
+            catch
+            {
+                _logger.LogError("Unable to check for updates for Omni Account Manager.");
+                return false;
+            }
+        }
+
+        public async Task Update()
+        {
+            try
+            {
+                using (var manager = await UpdateManager.GitHubUpdateManager(_endpoints.Github))
+                {
+                    await manager.UpdateApp();
+                    UpdateManager.RestartApp();
+                }
+            }
+            catch
+            {
+                _logger.LogError("Unable to update Omni Account Manager.");
+            }
+        }
+
+        public void Restart()
+        {
+            try
+            {
+                UpdateManager.RestartApp();
+            }
+            catch
+            {
+                _logger.LogError("Unable to restart Omni Account Manager.");
+            }
+        }
+    }
+}
