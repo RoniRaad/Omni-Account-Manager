2091abae54b71e180a610e3544cf18251226c89b
Roni <ronijraad@gmail.com>
2022-09-24 17:58:54 -0400

    Fixed user agent for riot client. Hopefully fixed update button.

--- AccountManager.Core/Models/RiotGames/Requests/InitialAuthTokenRequest.cs
+++ AccountManager.Core/Models/RiotGames/Requests/InitialAuthTokenRequest.cs
@@ -7,15 +7,6 @@ namespace AccountManager.Core.Models.RiotGames.Requests
     {
         [JsonPropertyName("client_id")]
         public string? Id { get; set; }
-        [JsonPropertyName("acr_values")]
-        public string? Acr { get; set; } = "";
-        [JsonPropertyName("claims")]
-        public string? Claims { get; set; } = "{\r\n    \"id_token\": {\r\n        \"rgn_NA1\": null\r\n    },\r\n    \"userinfo\": {\r\n        \"rgn_NA1\": null\r\n    }\r\n}";
-        [JsonPropertyName("code_challenge")]
-        public string? CodeChallenge { get; set; } = "";
-        [JsonPropertyName("code_challenge_method")]
-        public string? CodeChallengeMethod { get; set; } = "";
-        [JsonPropertyName("nonce")]
         public string? Nonce { get; set; }
         [JsonPropertyName("redirect_uri")]
         public string? RedirectUri { get; set; }
