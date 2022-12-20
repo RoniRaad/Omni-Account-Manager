f77b4b467bfd53be4e0f886471471d0a01cce6ef
Roni Raad <RoniRaad@users.noreply.github.com>
2022-12-20 06:22:58 -0500

    Added a number picker to league settings for match history count.

--- AccountManager.Core/Models/UserSettings/LeagueSettings.cs
+++ AccountManager.Core/Models/UserSettings/LeagueSettings.cs
@@ -1,4 +1,6 @@
-﻿namespace AccountManager.Core.Models.UserSettings
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AccountManager.Core.Models.UserSettings
 {
     public sealed class LeagueSettings
     {
@@ -6,5 +8,7 @@
 
         public bool UseAccountCredentials { get; set; } = true;
         public Guid? AccountToUseCredentials { get; set; }
+        [Range(1, 40, ErrorMessage = "Matches must be within (1-40).")]
+        public int AmountOfMatchesForGraphs { get; set; } = 20;
     }
 }
