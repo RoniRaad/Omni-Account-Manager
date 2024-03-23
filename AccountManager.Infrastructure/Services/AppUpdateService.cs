76000871eb131dc668e40ef0db586877451c9d7f
RoniRaad <RoniRaad@users.noreply.github.com>
2024-03-22 20:15:14 -0400

    started adding a downgrade process for the incorrectly pushed version.

--- AccountManager.Infrastructure/Services/AppUpdateService.cs
+++ AccountManager.Infrastructure/Services/AppUpdateService.cs
@@ -1,7 +1,10 @@
 ﻿using AccountManager.Core.Models.AppSettings;
+using CliWrap;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Squirrel;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace AccountManager.Infrastructure.Services
 {
@@ -21,7 +24,73 @@ namespace AccountManager.Infrastructure.Services
             {
                 #if DEBUG
                     _logger.LogError("Skipping update check due to the app running in debug mode.");
-                return false;
+				    using var manager2 = await UpdateManager.GitHubUpdateManager(_endpoints.Github);
+				    var currentVersion = manager2.CurrentlyInstalledVersion();
+                 
+				    // This is here temporarily to fix an accidental large jump in minor version number. TODO: remove this
+				if (currentVersion.Version.Minor == 91)
+				{
+                    var httpClient = new HttpClient();
+                    var getNugetPackage = await httpClient.GetAsync("https://github.com/RoniRaad/Omni-Account-Manager/releases/download/v1.19.2/OmniAccountManager-1.19.2-full.nupkg");
+                    if (!Directory.Exists("../tempDowngrade"))
+                    {
+                        Directory.CreateDirectory("../tempDowngrade");
+                    }
+                    using (FileStream nugetFile = File.OpenWrite("../tempDowngrade/OmniAccountManager-1.19.2-full.nupkg"))
+                    {
+                        using var contentStream = getNugetPackage.Content.ReadAsStream();
+                        await contentStream.CopyToAsync(nugetFile);
+                    }
+
+
+                    string exePath = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location) ?? ".", "..", "Update.exe");
+					string exeArguments = "--update=tempDowngrade";
+					string exeDirectory = Path.GetDirectoryName(exePath);
+
+                    try
+                    {
+						Directory.Delete(Path.Combine(exeDirectory, "packages"), true);
+					}
+                    catch
+                    {
+
+                    }
+
+					var downloadCorrectVersionStartInfo = new ProcessStartInfo(exePath, exeArguments)
+					{
+						UseShellExecute = false,
+						CreateNoWindow = true,
+						WindowStyle = ProcessWindowStyle.Hidden,
+                        WorkingDirectory = exeDirectory,
+					};
+
+					var process = Process.Start(downloadCorrectVersionStartInfo);
+                    await process.WaitForExitAsync();
+
+					string batchScript = Path.Combine(exeDirectory, "deleteDir.bat");
+					using (StreamWriter sw = new StreamWriter(batchScript))
+					{
+						sw.WriteLine("@echo off");
+						sw.WriteLine($"powershell.exe -Command \"Start-Sleep -Seconds 5; Remove-Item -Recurse -Force '{Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location)}'\""); // Wait for 5 seconds to ensure the app has closed
+						sw.WriteLine("start OmniAccountManager.exe"); // start the app
+						sw.WriteLine("powershell.exe -Command \"Remove-Item -Recurse -Force 'tempDowngrade'\""); // remove downgrade folder
+						sw.WriteLine("del \"%~f0\""); // Self-delete the batch script
+					}
+
+					// Configure and start the batch script process
+					ProcessStartInfo removeCurrentInstallStartInfo = new ProcessStartInfo(batchScript)
+					{
+						UseShellExecute = true,
+						CreateNoWindow = true,
+						WindowStyle = ProcessWindowStyle.Hidden,
+						WorkingDirectory = exeDirectory,
+					};
+
+					Process.Start(removeCurrentInstallStartInfo);
+                    manager2.KillAllExecutablesBelongingToPackage();
+				}
+
+				    return false;
                 #endif
                 using var manager = await UpdateManager.GitHubUpdateManager(_endpoints.Github);
                 var updateInfo = await manager.CheckForUpdate();
@@ -45,8 +114,20 @@ namespace AccountManager.Infrastructure.Services
         {
             try
             {
-                using var manager = await UpdateManager.GitHubUpdateManager(_endpoints.Github);
-                var releaseEntry = await manager.UpdateApp();
+
+				using var manager = await UpdateManager.GitHubUpdateManager(_endpoints.Github);
+				var currentVersion = manager.CurrentlyInstalledVersion();
+
+				// This is here temporarily to fix an accidental large jump in minor version number. TODO: remove this
+				if (currentVersion.Version.Minor == 91)
+				{
+					List<ReleaseEntry> releases = new List<ReleaseEntry>();
+					var httpClient = new HttpClient();
+					releases.AddRange(ReleaseEntry.ParseReleaseFile(await httpClient.GetStringAsync("https://github.com/RoniRaad/Omni-Account-Manager/releases/download/v1.91.2/OmniAccountManager-1.91.2-full.nupkg")));
+					await manager.DownloadReleases(releases);
+				}
+
+				var releaseEntry = await manager.UpdateApp();
                 var version = releaseEntry.Version;
                 var latestExePath = Path.Combine(manager.RootAppDirectory, string.Concat("app-", version.Version.Major, ".", version.Version.Minor, ".", version.Version.Build), "OmniAccountManager.exe");
 
